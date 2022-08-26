a64db2df458be3fffcf0e78a0010a1699847109c
flex728q09 <77707210+flex728q09@users.noreply.github.com>
2022-08-26 17:46:53 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -13,31 +13,31 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 // 👈 You Can change this your choice
-global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
+global.emoji01 = ['👑'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
+*Robin Ξ*
+*I am 19 years old developer...*
+*From Egypt*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
+*Follow My Facebook - https://www.facebook.com/rafaelarmia2 *`  // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+2011228108'] //👈  Enter Your number
+global.premium =  ['+201122810874'] //👈  Enter Your number
+global.ownernomer = '+201122810874' //👈  Enter Your number
+global.ownername = 'Robin' //👈 Enter Your name
+global.botname = 'Robin bot' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 Rafael' //👈 Enter Your Name
 global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
-global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
+global.region = 'EGYPT, cairo, shoubra' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
 global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
-global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
-global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
-global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
-global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
+global.myweb = 'https://www.facebook.com/rafaelarmia2'// 👈 Enter your Social media link to follow now button
+global.welcome = '*Welcome To The Group 🌚*' // 👈 You Can change this your choice
+global.bye = '*Bye bye 🌝*' // 👈 You Can change this your choice
+global.packname = 'ROBIN-BOTv8'  // 👈 You Can change this your choice 
+global.author = 'RAFAEL DEVELOPMENT' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
@@ -46,7 +46,7 @@ global.spoty = 'https://i.postimg.cc/HW2YbN1b/Pics-Art-22-08-09-16-12-14-489.png
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
 global.sp = '⭔'
 global.mess = {
-    success: 'Done 🦋', // 👈 You Can change this your choice
+    success: 'Done 🦇', // 👈 You Can change this your choice
     admin: 'This Feature Is Only For Admin!', // 👈 You Can change this your choice
     botAdmin: 'Bot Must Be Admin First!', // 👈 You Can change this your choice
     owner: 'This Feature Is Only For Owner!', // 👈 You Can change this your choice
@@ -67,7 +67,7 @@ global.sankey = "sanuwa"
 global.xteamkey = "nandowangy"
 global.carkey = "cakrayp24Q6"
 global.violkey = "beta"
-global.HunterApi = "FuckBitch"
+global.HunterApi = "bro"
 global.neokey = "V8qnSaTq"
 global.riy = "RiyGanz"
 
